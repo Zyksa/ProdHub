22a8ef25ba43575e5a8648ccba4c3574ab8b04d8
zyk <zyksa@protonmail.com>
2023-02-27 10:22:25 +0100

    add fields to query

--- src/components/Substance.jsx
+++ src/components/Substance.jsx
@@ -4,6 +4,20 @@ const Substance = ({data}) => {
         return (
             <div>
                 <h1 className="text-2xl">Informations about <span className="text-secondary">{ data.name}</span></h1>
+                <br/>
+                <p>Addiction potentielles : <span className="text-secondary">{ data.addictionPotential}</span> </p>
+                <br/>
+                <ul>
+                    <li>Classe chimique : <span className="text-secondary">{ data.class.chemical}</span></li>
+                    <li>Class psychoactive : <span className="text-secondary">{ data.class.psychoactive}</span></li>
+                </ul>
+
+                <br/>
+
+                <ul>
+                    <li>Tolérance : <span className="text-secondary">{ data.tolerance.full}</span></li>
+                    <li>Reset de la tolérance : <span className="text-secondary">{ data.tolerance.zero}</span></li>
+                </ul>
             </div>
 
         );
--- src/hooks/useGraphQLQuerie.js
+++ src/hooks/useGraphQLQuerie.js
@@ -9,40 +9,54 @@ export default function useGraphQLQuerie(substance) {
     {
 		substances(query: "${substance}") {
 			name
-			addictionPotential
-			class {
-				chemical
-				psychoactive
-			}
-			tolerance {
-				full
-				half
-				zero
-			}
-			# routes of administration
-			roas {
-				name
-				dose {
-					units
-					threshold
-					heavy
-					common { min max }
-					light { min max }
-					strong { min max }
-				}
-				duration {
-					afterglow { min max units }
-					comeup { min max units }
-					duration { min max units }
-					offset { min max units }
-					onset { min max units }
-					peak { min max units }
-					total { min max units }
-				}
-				bioavailability {
-					min max
-				}
-			}
+    		addictionPotential
+    		toxicity
+    		crossTolerances
+    		commonNames
+    		summary
+    		featured
+    
+        # routes of administration
+        roas {
+            name
+
+            dose {
+                units
+                threshold
+                heavy
+                common { min max }
+                light { min max }
+                strong { min max }
+            }
+
+            duration {
+                afterglow { min max units }
+                comeup { min max units }
+                duration { min max units }
+                offset { min max units }
+                onset { min max units }
+                peak { min max units }
+                total { min max units }
+            }
+
+            bioavailability {
+                min max
+            }
+        }
+
+        # subjective effects
+        effects {
+            name url
+        }
+    
+    	uncertainInteractions {
+      		name
+    	}
+    
+    	dangerousInteractions {
+			name
+		}
+    }
 		}
 	}`);
 
